a5175325f1c8b7ccb54cd9d47690f2c09f5d9d88
xuanduonghoa <duonghx.vnist@gmail.com>
2020-12-03 00:58:16 +0700

    giao diện học gõ chung + học gõ từ

--- src/views/learn/Learn.js
+++ src/views/learn/Learn.js
@@ -1,12 +1,138 @@
+import CIcon from "@coreui/icons-react"
 import { func } from "prop-types"
-import React from "react"
+import React, { Component } from "react"
+// import { cifAU } from '@coreui/icons';
+import {
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CContainer,
+  CCardText,
+  CCardSubtitle,
+  CCardTitle,
+  CRow,
+  CCol,
+  CInput,
+  CLabel,
+  CButton,
+  CCarousel,
+  CCarouselCaption,
+  CCarouselInner,
+  CCarouselIndicators,
+  CCarouselItem,
+  CCarouselControl,
+  CTabs,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CTabContent,
+  CTabPane
+} from "@coreui/react";
+function Learn() {
+  return (
+    <CContainer fluid>
+      <CRow>
+        <CCol sm="4">
+          <CCard className="text-black bg-secondary">
+            <CCardHeader >
+              Học gõ phím
+          </CCardHeader>
+            <CCardBody>
+              <CCardTitle>
+                Học gõ phím tốc ký
+            </CCardTitle>
+              <CCardText>
+                Học gõ phím tốc ký giúp bạn làm quen với bàn phím tốc ký tiếng Việt để có thể gõ thành thạo
+            </CCardText>
+            </CCardBody>
+            <CCardFooter className="text-black bg-secondary">
+              Bắt đầu vào học
+            <CIcon size={'xl'} name={'cil-arrow-right'} />
+            </CCardFooter>
+          </CCard>
+        </CCol>
+        <CCol sm="4">
+          <CCard className="bg-info">
+            <CCardHeader >
+              Học gõ âm
+          </CCardHeader>
+            <CCardBody>
+              <CCardTitle>
+                Học gõ âm tốc ký
+            </CCardTitle>
+              <CCardText>
+                Học gõ âm tiếng Việt tốc ký giúp bạn làm quen với các âm tiếng Việt trong khi gõ tốc ký
+            </CCardText>
+            </CCardBody>
+            <CCardFooter className="bg-info">
+              Bắt đầu vào học
+            <CIcon size={'xl'} name={'cil-arrow-right'} />
+            </CCardFooter>
+          </CCard>
+        </CCol>
 
-function Learn(){
-    return (
-        <div>
-            Hoa Xuân Dương
-        </div>
-        
-    );
+      </CRow>
+      <CRow>
+        <CCol sm="4">
+          <CCard className="bg-warning">
+            <CCardHeader >
+              Học gõ từ
+          </CCardHeader>
+            <CCardBody>
+              <CCardTitle>
+                Học gõ các từ
+            </CCardTitle>
+              <CCardText>
+                Học gõ các từ tiếng Việt giúp bạn học các quy tắc để tốc ký tiếng Việt
+            </CCardText>
+            </CCardBody>
+            <CCardFooter className="bg-warning">
+              Bắt đầu vào học
+            <CIcon size={'xl'} name={'cil-arrow-right'} />
+            </CCardFooter>
+          </CCard>
+        </CCol>
+        <CCol sm="4">
+          <CCard className="bg-success">
+            <CCardHeader >
+              Học gõ câu
+          </CCardHeader>
+            <CCardBody>
+              <CCardTitle>
+                Học gõ câu
+            </CCardTitle>
+              <CCardText>
+                Học gõ câu tiếng Việt tốc ký giúp bạn luyện tập gõ tốc ký để tăng tốc độ
+            </CCardText>
+            </CCardBody>
+            <CCardFooter className="bg-success">
+              Bắt đầu vào học
+            <CIcon size={'xl'} name={'cil-arrow-right'} />
+            </CCardFooter>
+          </CCard>
+        </CCol>
+        <CCol sm="4">
+          <CCard className="bg-primary">
+            <CCardHeader >
+              Từ của bạn
+          </CCardHeader>
+            <CCardBody>
+              <CCardTitle>
+              Từ của bạn
+            </CCardTitle>
+              <CCardText>
+                Nơi lưu giữ các âm, các từ, các câu để bạn có thể học, luyện tập.
+            </CCardText>
+            </CCardBody>
+            <CCardFooter className="bg-primary">
+              Bắt đầu vào học
+            <CIcon size={'xl'} name={'cil-arrow-right'} />
+            </CCardFooter>
+          </CCard>
+        </CCol>
+      </CRow>
+    </CContainer>
+  );
 }
 export default Learn
--- src/views/learn/LearnWord.js
+++ src/views/learn/LearnWord.js
@@ -1,12 +1,63 @@
+// import { CCard } from "@coreui/react";
 import { func } from "prop-types"
 import React from "react"
-
-function LearnWord(){
-    return (
-        <div>
-            Hoa Xuân Dương Word
-        </div>
-        
-    );
+import {
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCardHeader,
+  CContainer,
+  CCardText,
+  CCardSubtitle,
+  CCardTitle,
+  CRow,
+  CCol,
+  CInput,
+  CLabel,
+  CButton,
+  CCarousel,
+  CCarouselCaption,
+  CCarouselInner,
+  CCarouselIndicators,
+  CCarouselItem,
+  CCarouselControl,
+  CTabs,
+  CNav,
+  CNavItem,
+  CNavLink,
+  CTabContent,
+  CTabPane,
+  CWidgetBrand,
+  CSelect
+} from "@coreui/react";
+import { icons } from "src/assets/icons";
+function LearnWord() {
+  return (
+    <CContainer fluid>
+      <CRow>
+        <CCol sm="2">
+          <CButton color="info">Từ của bạn</CButton>
+        </CCol>
+        <CCol sm="4">
+          <CSelect>
+            <option>Học từ bất kỳ</option>
+            <option>Học từ âm chính</option>
+            <option>Học từ âm chính + âm cuối</option>
+            <option>Học từ âm đầu + âm chính</option>
+            <option>Học từ âm đầu + âm chính + âm cuối</option>
+          </CSelect>
+        </CCol>
+      </CRow>
+      <CRow>
+      <CContainer>
+      <CCol sm="2">
+          <CContainer >
+OA
+          </CContainer>
+        </CCol>
+      </CContainer>
+      </CRow>
+    </CContainer>
+  );
 }
 export default LearnWord
