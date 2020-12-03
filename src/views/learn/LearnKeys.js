dc3abd34533e3e9307033d9c2bba3e83a04c4dde
ducanh <ducanhhust99@gmail.com>
2020-12-03 13:24:52 +0700

    learn keys

--- src/views/learn/LearnKeys.js
+++ src/views/learn/LearnKeys.js
@@ -11,14 +11,80 @@ import {
   CCol,
   CButton,
   CSelect,
+  CLink,
+  CInput,
+  CLabel,
+  CCallout,
+  CWidgetSimple,
+  CAlert
 } from "@coreui/react";
 import { icons } from "src/assets/icons";
+
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 function LearnKeys() {
   return (
     <CContainer fluid>
       <CCard>
-        <CCardHeader></CCardHeader>
-        <CCardBody></CCardBody>
+        <CCardHeader>
+          <CLink to="/learn/your_word">
+            <CButton color="primary">Từ của bạn</CButton>
+          </CLink>
+        </CCardHeader>
+        <CCardBody>
+          <CRow>
+            <CCol sm="8">
+              <CCard>
+                <CCardBody>
+                  <CCardBody>
+                    <CRow>
+                      <CCol sm="5">
+                        <CWidgetSimple text="OA"></CWidgetSimple>
+                      </CCol>
+                      <CCol sm="5">
+                        <CWidgetSimple text="-C"></CWidgetSimple>
+                      </CCol>
+                      <CCol sm="2">
+                        <CButton color="info">Tiếp</CButton>
+                      </CCol>
+                    </CRow>
+                  </CCardBody>
+                  <CAlert color="success">Chính xác</CAlert>
+                  <CInput />
+                </CCardBody>
+              </CCard>
+            </CCol>
+            <CCol sm="4">
+              <CCard>
+                <CCardBody>
+                  <CLabel>Tỷ lệ gõ đúng</CLabel>
+                  <CCallout color="info" className={"bg-white"}>
+                    <strong className="h4">45%</strong>
+                  </CCallout>
+                  <br />
+                  <CLabel> Thời gian còn lại</CLabel>
+                  <CCallout color="info" className={"bg-white"}>
+                    <strong className="h4">00:20</strong>
+                  </CCallout>
+                </CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+          <CRow>
+            <CCol sm="8">
+              <CCard>
+                <CCardBody style={{ height: "300px" }}>
+                  <Keyboard />
+                </CCardBody>
+              </CCard>
+            </CCol>
+            <CCol sm="4">
+              <CCard>
+                <CCardBody style={{ height: "300px" }}></CCardBody>
+              </CCard>
+            </CCol>
+          </CRow>
+        </CCardBody>
       </CCard>
     </CContainer>
   );
