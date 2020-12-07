f61554d19aea4b5189fe6a921dcb836ce034de60
xuanduonghoa <duonghx.vnist@gmail.com>
2020-12-08 01:11:42 +0700

    chart and dashboard

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -17,20 +17,54 @@ import {
   CLink,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
-import ApexCharts from 'apexcharts'
+import ApexCharts from "apexcharts";
 import ApexChart from "./chart";
-import ReactApexChart from 'apexcharts';
-import Chart from 'react-apexcharts';
+import ReactApexChart from "apexcharts";
+import Chart from "react-apexcharts";
 const ChartWord = React.lazy(() => import("./chartWord"));
 const ChartSpeed = React.lazy(() => import("./chartSpeed"));
 const ChartRating = React.lazy(() => import("./chartRating"));
-
-const TimeChart = () => {
+const Progress = (props) => {
   return (
-    <div id="chart">
-    </div>
+    <CProgress
+      value={props.text}
+      showValue
+      className="mb-1 bg-grey"
+      color={props.progress}
+      style={{ width: "100%", margin: "auto" }}
+    />
   );
-}
+};
+const LearnResult = () => {
+  return (
+    // <CCard>
+    //   <CCardHeader>
+    //     <CCardTitle></CCardTitle>
+    //   </CCardHeader>
+    //   <CCardBody>
+    //     <CRow>
+    //     <Progress></Progress>
+    //     <Star></Star>
+    //     </CRow>
+
+    //   </CCardBody>
+    //   <CCardFooter></CCardFooter>
+    // </CCard>
+    <CContainer>
+      <CRow>
+        <CCol sm="4">
+          <CCardTitle>Học gõ phím</CCardTitle>
+        </CCol>
+        <CCol sm="4">
+          <Progress text="20" progress="success" />
+        </CCol>
+        <CCol sm="4">
+          <Star></Star>
+        </CCol>
+      </CRow>
+    </CContainer>
+  );
+};
 const Star = (props) => {
   return (
     <CRow style={{ textAlign: "center" }}>
@@ -77,37 +111,45 @@ class Dashboard extends React.Component {
     super(props);
 
     this.state = {
-
-      series: [{
-        name: 'series1',
-        data: [31, 40, 28, 51, 42, 109, 100]
-      }, {
-        name: 'series2',
-        data: [11, 32, 45, 32, 34, 52, 41]
-      }],
+      series: [
+        {
+          name: "Thời gian học gõ",
+          data: [31, 40, 28, 51, 42, 109, 100],
+        },
+        {
+          name: "Tốc độ gõ",
+          data: [11, 32, 45, 32, 34, 52, 41],
+        },
+      ],
       options: {
         chart: {
-          height: 350,
-          type: 'area'
+          height: 400,
+          type: "area",
         },
         dataLabels: {
-          enabled: false
+          enabled: false,
         },
         stroke: {
-          curve: 'smooth'
+          curve: "smooth",
         },
         xaxis: {
-          type: 'datetime',
-          categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+          type: "datetime",
+          categories: [
+            "2020-08-11T00:00:00.000Z",
+            "2020-08-12T01:30:00.000Z",
+            "2020-08-12T02:30:00.000Z",
+            "2020-08-13T03:30:00.000Z",
+            "2020-08-13T04:30:00.000Z",
+            "2020-08-14T05:30:00.000Z",
+            "2020-08-14T06:30:00.000Z",
+          ],
         },
         tooltip: {
           x: {
-            format: 'dd/MM/yy HH:mm'
+            format: "dd/MM/yy HH:mm",
           },
         },
       },
-
-
     };
   }
   render() {
@@ -115,63 +157,73 @@ class Dashboard extends React.Component {
       <CContainer fluid>
         <CRow>
           <CCol sm="8">
-            <CRow>
-              <CCard>
-                <CCardHeader></CCardHeader>
-                <CCardTitle></CCardTitle>
-                <CCardBody>
-                  {/* <ChartWord/> */}
-                </CCardBody>
-                <CCardFooter></CCardFooter>
-              </CCard>
-            </CRow>
-            <CRow>
-                <Chart options={this.state.options} series={this.state.series} type="area" height={350} />
-            </CRow>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Đánh giá kết quả học gõ tốc ký</CCardTitle>
+              </CCardHeader>
+
+              <CCardBody>
+                <Chart
+                  options={this.state.options}
+                  series={this.state.series}
+                  type="area"
+                  height={400}
+                />
+              </CCardBody>
+            </CCard>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Đánh giá quá trình học gõ tốc ký</CCardTitle>
+              </CCardHeader>
+
+              <CCardBody>
+                <LearnResult></LearnResult>
+                <LearnResult></LearnResult>
+                <LearnResult></LearnResult>
+                <LearnResult></LearnResult>
+              </CCardBody>
+            </CCard>
           </CCol>
           <CCol sm="4">
-            <CRow>
-              {/* <ChartWord />
+            {/* <ChartWord />
               <ChartSpeed />
               <ChartRating /> */}
-              <CCard>
-                <CCardHeader>
-                  <CCardTitle>Các bài đã học gần đây</CCardTitle>
-                </CCardHeader>
-                <CCardBody>
-                  <LessonShort
-                    text="Bài 1: Học các phím tay trái"
-                    linkLearn=""
-                    linkPractice=""
-                  />
-                  <LessonShort
-                    text="Bài 1: Học các phím tay trái"
-                    linkLearn=""
-                    linkPractice=""
-                  />
-                  <LessonShort
-                    text="Bài 1: Học các phím tay trái"
-                    linkLearn=""
-                    linkPractice=""
-                  />
-                  <LessonShort
-                    text="Bài 1: Học các phím tay trái"
-                    linkLearn=""
-                    linkPractice=""
-                  />
-                  <CContainer>
-                    <CLink>Xem thêm</CLink>
-                  </CContainer>
-                </CCardBody>
-                <CCardFooter></CCardFooter>
-              </CCard>
-            </CRow>
+            <CCard>
+              <CCardHeader>
+                <CCardTitle>Các bài đã học gần đây</CCardTitle>
+              </CCardHeader>
+              <CCardBody>
+                <LessonShort
+                  text="Bài 1: Học các phím tay trái"
+                  linkLearn=""
+                  linkPractice=""
+                />
+                <LessonShort
+                  text="Bài 1: Học các phím tay trái"
+                  linkLearn=""
+                  linkPractice=""
+                />
+                <LessonShort
+                  text="Bài 1: Học các phím tay trái"
+                  linkLearn=""
+                  linkPractice=""
+                />
+                <LessonShort
+                  text="Bài 1: Học các phím tay trái"
+                  linkLearn=""
+                  linkPractice=""
+                />
+                <CContainer>
+                  <CLink>Xem thêm</CLink>
+                </CContainer>
+              </CCardBody>
+              <CCardFooter></CCardFooter>
+            </CCard>
           </CCol>
         </CRow>
       </CContainer>
     );
   }
-
-};
+}
 
 export default Dashboard;
