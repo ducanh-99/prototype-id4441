fac020d54693183d4d084ebee6e63e2beb70fe8b
xuanduonghoa <duonghx.vnist@gmail.com>
2020-12-08 09:52:29 +0700

    your word

--- src/views/learn/YourWord.js
+++ src/views/learn/YourWord.js
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import $ from 'jquery'
 import {
   CCard,
   CCardBody,
@@ -13,9 +13,62 @@ import {
   CProgress,
   CButton,
   CDataTable,
+  CModal,
+  CModalHeader,
+  CModalBody,
+  CModalFooter,
+  CInput,
+  CLabel,
+  CSelect,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 
+function onchangeSelect() {
+  $('#vnInput').on('change', function(){
+    console.log("hihi", $('#vnInput').val())
+    $('#stenoOutput').val($('#vnInput').val());
+  })
+}
+const Modal = (props) => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+  return (
+    <CContainer>
+      <CButton onClick={toggle} color="info">
+        {props.text}
+      </CButton>
+      <CModal show={modal} onClose={toggle}>
+        <CModalHeader closeButton>Thêm mới từ của bạn</CModalHeader>
+        <CModalBody>
+          <CRow>
+            <CCol sm="5">
+              <CLabel>Từ tiếng Việt muốn thêm</CLabel>
+              <CSelect id="vnInput" onChange={onchangeSelect()}>
+                <option>Chọn từ tiếng Việt</option>
+                <option value="H-U">hoa</option>
+                <option value="ST*-YN">xuân</option>
+                <option value="SP-EWG">dương</option>
+              </CSelect>
+            </CCol>
+            <CCol sm="5">
+              <CLabel>Từ tốc ký tương ứng</CLabel>
+              <CInput disabled="true" id="stenoOutput"></CInput>
+            </CCol>
+          </CRow>
+        </CModalBody>
+        <CModalFooter>
+          <CButton color="primary" onClick={toggle}>Thêm</CButton>{" "}
+          <CButton color="secondary" onClick={toggle}>
+            Hủy
+          </CButton>
+        </CModalFooter>
+      </CModal>
+    </CContainer>
+  );
+};
 const fields = [
   { key: "key", label: "STT" },
   { key: "word", label: "Danh sách từ câu", _style: { width: "70%" } },
@@ -48,15 +101,18 @@ function YourWord() {
       <CContainer>
         <CCard>
           <CCardHeader>
-            <CButton color="primary" style={{ marginLeft: "10px" }}>
-              Xem quy tắc
-            </CButton>
-            <CButton color="success" style={{ marginLeft: "10px" }}>
-              Luyện các từ đã chọn
-            </CButton>
-            <CButton color="danger" style={{ marginLeft: "10px" }}>
-              Xóa các từ đã chọn
-            </CButton>
+            {/* <CButton color="primary" style={{ marginLeft: "10px" }} onClick="">
+              Thêm từ của bạn
+            </CButton> */}
+            <CRow>
+              <Modal text="Thêm từ của bạn"></Modal>
+              <CButton color="success" style={{ marginLeft: "10px" }}>
+                Luyện các từ đã chọn
+              </CButton>
+              <CButton color="danger" style={{ marginLeft: "10px" }}>
+                Xóa các từ đã chọn
+              </CButton>
+            </CRow>
           </CCardHeader>
           <CCardBody>
             <CDataTable
